244a15f8dcd219590bd1edefc37edb9ee5518834
hectormamani18 <Hectormamani18@hotmail.com>
2022-10-01 20:24:11 -0300

    Desafio 1

---
+++ Clases-02/Clases.js
@@ -0,0 +1,50 @@
+class   Usuarios {
+    constructor(nombre,apellidos){
+    this.nombre=nombre
+    this.apellidos=apellidos
+    this.libros=[];
+    this.mascotas=[];
+    }
+    getFullName() {
+    return `${this.nombre} ${this.apellidos}`
+   }
+  
+    addMascotas(nuevaMascota){
+       this.mascotas.push(nuevaMascota);
+       
+    }
+
+    countMascotas(){
+        const cant = this.mascotas.length;
+    return cant
+    }
+    
+    addBook(nombre,autor){
+        const book ={
+            nombre: nombre,
+            autor:autor
+        }
+        this.libros.push(book)
+          
+    }
+    getBookNames(){
+        let bookName=[]
+        this.libros.map((libro)=>bookName.push(libro.nombre));
+        return bookName;
+      
+    }
+    
+};
+
+
+const usuario = new Usuarios("Hector","Mamani");
+
+console.log(usuario.getFullName());
+usuario.addMascotas("Conejo");
+usuario.addMascotas("Perro");
+console.log(usuario.countMascotas())
+
+usuario.addBook("Misery","Stephen King");
+usuario.addBook("it","Stephen King");
+console.log(usuario.getBookNames());
+
