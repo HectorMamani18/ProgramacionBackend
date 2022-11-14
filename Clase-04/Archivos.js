6f618830b9b03ef05511f57db3ef82bbfe0afdf8
hectormamani18 <Hectormamani18@hotmail.com>
2022-11-14 00:35:06 -0300

    Correccion realizada

--- Clase-04/Archivos.js
+++ Clase-04/Archivos.js
@@ -1,93 +1,110 @@
 const fs = require('fs')
 
-const myJson ='Archivo.json'
+const nameJson ='Archivo.json'
+class Contenedor {
 
-const productosJson= async()=>{
- const data = await  fs.promises.readFile(myJson,'utf-8') 
- return JSON.parse(data);  
-}
-const guardarProduts = async (productos) => {
-	const dato = JSON.stringify(productos, null, '\t')
-	await fs.promises.writeFile(myJson, dato)
-};
-
-const getAll = async()=>{
-    const productos = await productosJson();
-    return productos;
-}
-
-const getById = async(id)=>{
-    const productos = await productosJson();
-    
-    const findId = productos.findIndex((producto)=>producto.id === id);
-    if(findId < 0){
-        throw new Error('Producto no Encontrado')
-    }
-    return  productos[findId]
-}
- const save = async (data)=>{
-    if(!data.title || !data.price || typeof data.title !== 'string' || typeof data.price !== 'number') throw new Error('Datos invalidos');
-    
-    const productos = await productosJson();
-    let id = 1;
-    if (productos.length){
-        id = productos[productos.length -1].id +1
-    }
-    const nuevoProducto = {
-		title: data.title,
-		price: data.price,
-		id: id
-	}
-
-	productos.push(nuevoProducto);
-
-	await guardarProduts(productos)
-        
+    constructor(nameJson) {
+        this.nameJson = nameJson
     }
 
-    const deleteAll = async ()=>{
-        await guardarProduts([]);
-
+    async validateData() {
+        const exits = await fs.promises.stat(this.nameJson)
+        !exits ? await fs.promises.writeFile(this.nameJson, JSON.stringify(['Error 404'])) : null
     }
-    const deleteById = async (id)=>{
-        const productos = await productosJson();
-        const findId = productos.findIndex((producto)=>producto.id === id)
-        if(findId < 0){
-            return;
+
+    async getProducts() {
+        await this.validateData()
+        const data = await fs.promises.readFile(this.nameJson, 'utf-8')
+        return JSON.parse(data)
+    }
+
+    async log() {
+        const products = await this.getProducts()
+        console.log(products)
+    }
+
+    async save(data) {
+        if (!data.title || !data.price || typeof data.title !== 'string' || typeof data.price !== 'number') throw new Error('Datos Invalidos')
+
+        const products = await this.getProducts()
+
+        const newProduct = {
+            title: data.title,
+            price: data.price,
+            id: products[products.length -1].id + 1
         }
-        productos.splice(findId,1)
+
+        products.push(newProduct)
+
+        await this.saveProducts(products)
+    }
+
+    async saveProducts(products) {
+        const data = JSON.stringify(products, null , '\t')
+        await fs.promises.writeFile(this.nameJson, data)
+    }
+
+    async getById(idBuscado) {
+        const products = await this.getProducts()
+
+        const index = products.findIndex((product) => product.id === idBuscado)
+
+        if (index < 0) throw new Error('El producto no existe')
+
+        return products[index]
+    }
+
+    async getAll() {
+        const products = await this.getProducts()
+        return products
+    }
+
+    async deleteById(idDeleted) {
+        const products = await this.getProducts()
+
+        const index = products.findIndex((product) => product.id === idDeleted);
+
+        if (index < 0) return;
+
+        products.splice(index,1)
+        
+        await this.saveProducts(products)
+    }
+
+}
+
+const main = async () => {
+    const contenedor = new Contenedor(nameJson)
+    
+    // Save Product
+    await contenedor.save({
+        title: 'Smoking',
+        price: 32000
+    })
+
+    // Get All Products
+    const products = await contenedor.getAll()
+    console.log(products)
+
+    // Get Product by Id
+    const product = await contenedor.getById(1)
+    console.log(product)
+
+    // Delete Product by Id
+    await contenedor.deleteById(2)
+    await contenedor.log()
+    
+}
+
+main()               
 
 
-        await guardarProduts(productos)
-
- }
- 
 
 
 
-
-                              //Llamado a getAll//
-
-//getAll().then((data)=>{console.log(data)}) 
-
-
-
-                              //Llamado a getById//
-/* const productoid=async ()=>{
-    const data = await getById(2);
-    console.log(data)
-}                              
- productoid(1);  */                            
+                          
                               
-                              //Llamado a save//
- /* const guardar =async ()=>{
-    await save({
-        title: "Estilo Tradicional",
-        price: 1500,     
-        id: 4
-    });} 
- guardar();   */    
  
-                     //Llamado a deleteById y deleteAll/
-//deleteAll()                  
-//deleteById(3)                    
+  
+ 
+            
